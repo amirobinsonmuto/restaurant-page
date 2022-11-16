0026413a8211886792ebb0e8e8032ec1653ae297
AmiRobinsonMuto <amimuto1002@gmail.com>
2022-11-16 15:31:53 -0800

    Init commit

--- webpack.config.js
+++ webpack.config.js
@@ -1,17 +1,19 @@
 const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
 
   entry: './src/index.js',
 
+  plugins: [
+    new HtmlWebpackPlugin({
+      title: 'Restaurant',
+    }),
+  ],
+
   output: {
     filename: 'main.js',
     path: path.resolve(__dirname, 'dist'),
-    clean: true,
-
-  devtool: 'hidden-source-map'
-
-  mode: 'development';
 
   },
 };
